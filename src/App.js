2e9cde35559f771f178845275c0be77e979e6a26
YadhuM21 <yadhurishnam@gmail.com>
2021-09-20 17:05:14 +0530

    Vesion 1

---
+++ src/App.js
@@ -0,0 +1,75 @@
+// import logo from "./logo.svg";
+import React, { useState } from 'react'
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [btnName, setBtnName] = useState('Enable Dark Mode')
+
+  const toggleMode=() => {
+    handleDarkMode();
+    if (mode === "dark") {
+      setMode("light");
+      setBtnName("Enable Dark Mode");
+      document.body.style.backgroundColor = 'white';
+    } else {
+      setMode("dark");
+      setBtnName("Turn Off Dark Mode");
+      document.body.style.backgroundColor = '#042743';
+    }
+  }
+
+  const handleDarkMode = () => {
+    if (darkObj.color === "black") {
+      setdarkObj({
+        color: "white",
+        backgroundColor: "black",
+        border: "0.5px solid #0b1eac",
+      });
+    } else {
+      setdarkObj({
+        color: "black",
+        backgroundColor: "white",
+      });
+    }
+  };
+  const [darkObj, setdarkObj] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+
+  return (
+    <>
+    <Router>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} btnName={btnName}></Navbar>
+
+      <div className="container my-3">
+      <Switch>
+
+          <Route path="/about">
+            <About darkObj={darkObj}/>
+          </Route>
+
+          <Route path="/">
+          <TextForm heading="Enter the Text to Analyze" mode={mode}/>
+          </Route>
+
+        </Switch>
+      {/* <About/> */}
+      </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClear = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  // const handlespaces =()=>{
+  //   let newtext = text.trim();
+  //   setText(newtext);
+  //   };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            placeholder="Enter the text here"
+            value={text}
+            onChange={handleOnChange}
+            style={{color: props.mode==='dark'?'white':'black' , backgroundColor: props.mode==='dark'?'#042743':'white'}}
+            // style={{color: props.mode==='dark'?'white':'black'}}
+            id="myBox"
+            rows="8"
+          ></textarea>
+          <button className="btn btn-primary my-3 mx-3" onClick={handleUpClick}>
+            Convert to UpperCase
+          </button>
+          <button
+            className="btn btn-success my-3 mx-3"
+            onClick={handleLowClick}
+          >
+            Convert to LowerCase
+          </button>
+          <button className="btn btn-danger my-3 mx-3" onClick={handleClear}>
+            {" "}
+            CLEAR{" "}
+          </button>
+          {/* <button onClick={handleDarkMode} className="btn btn-primary my-3">
+            {buttonName}
+          </button> */}
+          {/* <button className="btn btn-warning my-3 mx-3" onClick={handlespaces}> Clear Space </button> */}
+        </div>
+      </div>
+      <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>Text Summary</h1>
+        <p>
+          {text.split(" ").length} words , {text.length} charecters
+        </p>
+        <h2>Preveiw</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
