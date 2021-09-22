8184ad0f1c69e5ec9a4c6cc4e7c4525a2f794ae6
YadhuM21 <yadhurishnam@gmail.com>
2021-09-22 14:25:30 +0530

    Updated href(/ was changed to #)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,14 +10,14 @@ export default function Navbar(props) {
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${(props.mode==='light' ? 'light': 'black')} bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" style={{color: props.mode==='light'?"black":"white"}} href="/">{props.title}</a>
+    <a className="navbar-brand" style={{color: props.mode==='light'?"black":"white"}} href="#">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" style={{color: props.mode==='light'?"black":"white"}} aria-current="page" href="/">Home</a>
+          <a className="nav-link active" style={{color: props.mode==='light'?"black":"white"}} aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" style={{color: props.mode==='light'?"black":"white"}} href="/about">{props.aboutText}</a>
